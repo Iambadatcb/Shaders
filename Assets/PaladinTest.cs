7010b3f750e86c134b02a7f37e0680e9d82b64cf
Iambadatcb <gviduks.cepuliuks@gmail.com>
2025-03-02 13:08:29 +0200

    s

---
+++ Assets/PaladinTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PaladinTest : MonoBehaviour
+{
+    public ParticleSystem slashEffect;
+
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            StartCoroutine(Slash());
+        }
+    }
+
+    IEnumerator Slash()
+    {
+        anim.Play("Slash");
+        yield return new WaitForSeconds(0.8f);
+        slashEffect.Play();
+    }
+}
